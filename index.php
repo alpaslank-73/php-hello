ad3c439de86020bb635f4aaf9079c0ff3ac72301
Student User <student@workstation.lab.example.com>
2023-02-01 05:22:46 -0500

    index.php sadelesti

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
 <html>
   <head>
     <title>Hello World!</title>
-    <style>
-      h1 {
-        text-align: center;
-      }
-      h2 {
-        text-align: left;
-      }
-      h3 {
-        text-align: right;
-      }
 </style>
-  </head>
+ </head>
   <body>
-    <?php
-  print "Hello from " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
-    ?>
+   <?php print "Hello from " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n"; ?>
   </body>
 </html>
