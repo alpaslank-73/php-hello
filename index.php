5ef5509815e1380ce016d8e4e18ac832043dd7c8
Student User <student@workstation.lab.example.com>
2023-02-01 05:24:12 -0500

    daha da sadelesti index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <html>
-  <head>
-    <title>Hello World!</title>
-</style>
- </head>
-  <body>
-   <?php print "Hello from " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n"; ?>
-  </body>
+ <body>
+ <?php print "Hello from " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n"; ?>
+ </body>
 </html>
