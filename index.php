4ee28b1cee778c6e902d7dee7a9c1d772b041b72
alpaslank-73 <59145937+alpaslank-73@users.noreply.github.com>
2024-12-11 11:40:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
  <body>
- <?php print "v6.0 - Hello from " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n"; ?>
+ <?php print "v7.0 - Hello from " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n"; ?>
  </body>
 </html>
